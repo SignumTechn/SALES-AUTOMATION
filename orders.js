76d085cc64cd4e4103d4ff8343bc79916bcadd54
ROYALTY CODES KENYA <115067913+ROYALTY-CODES-254@users.noreply.github.com>
2023-07-01 11:03:26 +0300

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'Folderble Bicycle',
+        productNumber: '85631',
+        paymentStatus: 'due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'Larvender KF102 Drone',
+        productNumber: '365631',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko f5 helicopter',
+        productNumber: '49631',
+        paymentStatus: 'due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'F22 Raptor',
+        productNumber: '85631',
+        paymentStatus: 'paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Ak 47 infantry arm',
+        productNumber: '22631',
+        paymentStatus: 'paid',
+        shipping: 'Delivered'
+    },
+]
